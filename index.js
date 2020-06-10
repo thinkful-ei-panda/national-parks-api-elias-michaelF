3be5c514255f194efe1ac63ceea1e43317b561ef
mdotfal <michael.fallorina@gmail.com>
2020-06-09 20:40:27 -0700

    Reverted to 6b07c56c389e34a4259dd5ccaeac8c5f06047cbd

--- index.js
+++ index.js
@@ -2,51 +2,46 @@
 //api url: https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=kHxGnN12wErlgIBuQccdlyjRpZEtvtQYfS8cqa1I
 
 function htmlTemplate(name, des, url) {
-  console.log('template is getting created');
   //Generates HTML Template
   return `<li> 
-    <p>${name}</p>
+    <h3>${name}</h3>
     <p>${des}</p>
-    <p>${url}</p>
+    <p><a href="${url}">${url}</a></p>
   </li>
  `;
 }
 function printList(data) {
-  console.log("start printing");
   //Renders HTML Template
-  $('#results2').html(
+  $('.results').html(
     data.map((park) => htmlTemplate(park.fullName, park.description, park.url))
   );
-  $('#results').removeClass('hidden');
-  console.log('printing out the list');
 }
 
 function callAPI(states, num) {
-  console.log('calling all APIs');
-  const stateQuery = 'stateCode='+ states.replace(' ','');
-  const apiKey = 'kHxGnN12wErlgIBuQccdlyjRpZEtvtQYfS8cqa1I';
+  const stateQuery = 'stateCode=' + states;
+  const apiKey = 'api_key=kHxGnN12wErlgIBuQccdlyjRpZEtvtQYfS8cqa1I';
   const limit = 'limit=' + num;
   fetch(
-    `https://developer.nps.gov/api/v1/parks?${stateQuery}&${limit}&api_key=${apiKey}`
+    `https://developer.nps.gov/api/v1/parks?${stateQuery}&${limit}&${apiKey}`
   )
-    .then((response) =>
-      response.ok ? response.json() : Promise.reject('Cannot fetch parks data')
+    .then((res) =>
+      res.ok ? res.json() : Promise.reject('Cannot fetch parks data')
     )
-    .then((response) => printList(response.data));
+    .then((res) => printList(res.data));
 }
 
 function searchParks() {
   // Listens to when user clicks submit
-  console.log('entered something, well thats good buddy');
-  $('#js-form').submit(function (event) {
+  $('.js-form').submit(function (event) {
+    console.log('search is running');
     event.preventDefault();
-    let states = $('#js-states').val();
-    let numResults = $('#js-numOfResult').val();
+    let states = $('.js-states').val();
+    let numResults = $('.js-numOfResult').val();
     callAPI(states, numResults.toString());
   });
 }
-
 function watchForm() {
   searchParks();
 }
+
 $(watchForm);
