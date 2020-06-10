6b07c56c389e34a4259dd5ccaeac8c5f06047cbd
Elias Martinez <jemart218@gmail.com>
2020-06-09 17:02:36 -0700

    good version of the app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //api url: https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=kHxGnN12wErlgIBuQccdlyjRpZEtvtQYfS8cqa1I
 
 function htmlTemplate(name, des, url) {
+  console.log('template is getting created');
   //Generates HTML Template
   return `<li> 
     <p>${name}</p>
@@ -11,33 +12,40 @@ function htmlTemplate(name, des, url) {
  `;
 }
 function printList(data) {
+  console.log("start printing");
   //Renders HTML Template
-  $('#results').html(
+  $('#results2').html(
     data.map((park) => htmlTemplate(park.fullName, park.description, park.url))
   );
   $('#results').removeClass('hidden');
+  console.log('printing out the list');
 }
+
 function callAPI(states, num) {
-  const stateQuery = 'stateCode=' + states;
+  console.log('calling all APIs');
+  const stateQuery = 'stateCode='+ states.replace(' ','');
   const apiKey = 'kHxGnN12wErlgIBuQccdlyjRpZEtvtQYfS8cqa1I';
   const limit = 'limit=' + num;
   fetch(
-    `https://developer.nps.gov/api/v1/parks?${stateQuery}&${limit}&${apiKey}`
+    `https://developer.nps.gov/api/v1/parks?${stateQuery}&${limit}&api_key=${apiKey}`
   )
     .then((response) =>
       response.ok ? response.json() : Promise.reject('Cannot fetch parks data')
     )
     .then((response) => printList(response.data));
 }
+
 function searchParks() {
   // Listens to when user clicks submit
-  $('.js-form').submit(function (event) {
+  console.log('entered something, well thats good buddy');
+  $('#js-form').submit(function (event) {
     event.preventDefault();
-    let states = $('.js-states').val();
-    let numResults = $('.js-numOfResult').val();
+    let states = $('#js-states').val();
+    let numResults = $('#js-numOfResult').val();
     callAPI(states, numResults.toString());
   });
 }
+
 function watchForm() {
   searchParks();
 }
